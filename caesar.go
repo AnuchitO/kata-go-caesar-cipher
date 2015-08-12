c925d0fdbf649c20cc4959690e658bb2345c84cd
Pead Nong <anuchit.prasertsang@gmail.com>
2015-08-12 11:53:02 +0700

    refactor without hard code

--- caesar.go
+++ caesar.go
@@ -1,17 +1,6 @@
 package main
 
 func Caesar(text string, shift int) string {
-	if shift == 1 {
-		return "B"
-	}
-
-	if shift == 2 {
-		return "C"
-	}
-
-	if shift == 3 {
-		return "D"
-	}
-
-	return "E"
+	b := []byte(text)
+	return string(b[0] + byte(shift))
 }
