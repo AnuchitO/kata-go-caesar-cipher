c0292f45e59d6463af54dc2b7e0f41b7416259ca
Pead Nong <anuchit.prasertsang@gmail.com>
2015-08-12 15:30:49 +0700

    refactor add into slice

--- caesar.go
+++ caesar.go
@@ -4,7 +4,7 @@ func Caesar(text string, shift int32) string {
 	var b []byte
 
 	for _, c := range text {
-		b = append(b, byte(65+(c+shift-65)%26))
+		b = append(b, byte('A'+(c+shift-'A')%26))
 	}
 
 	return string(b)
