1efe4b10a16732a1aa4598e3c730f412cfc6e75a
Pead Nong <anuchit.prasertsang@gmail.com>
2015-08-12 13:54:34 +0700

    reduce to for loop

--- caesar.go
+++ caesar.go
@@ -3,20 +3,9 @@ package main
 func Caesar(text string, shift int) string {
 	b := []byte(text)
 
-	if len(b) == 1 {
-		return string(b[0] + byte(shift))
+	for index := 0; index < len(b); index++ {
+		b[index] = b[index] + byte(shift)
 	}
-
-	if len(b) == 2 {
-		b[0] = b[0] + byte(shift)
-		b[1] = b[1] + byte(shift)
-
-		return string(b)
-	}
-
-	b[0] = b[0] + byte(shift)
-	b[1] = b[1] + byte(shift)
-	b[2] = b[2] + byte(shift)
 
 	return string(b)
 }
